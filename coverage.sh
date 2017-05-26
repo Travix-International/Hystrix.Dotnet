6183c18baf4c2ad36c7cfd81e857965178d29796
Mark Vincze <mvincze@travix.com>
2017-05-26 17:08:07 +0200

    Upgrade the solution to use the new csproj-based dotnet tooling

--- coverage.sh
+++ coverage.sh
@@ -19,11 +19,11 @@ echo CLI args: $DOTNET_BUILD_ARGS
 
 echo Restoring
 
-dotnet restore -v Warning
+dotnet restore
 
 echo Building
 
-dotnet build $DOTNET_BUILD_ARGS **/project.json
+dotnet build $DOTNET_BUILD_ARGS
 
 echo Testing
 
@@ -37,20 +37,23 @@ do
   # We need to get the name of the project, to know what include in the OpenCover filter, so we have to extract the "Hystrix.Dotnet" part from the path string.
   project=`echo $testdir | cut -d/ -f2 | sed -e 's/\(\.UnitTests\)*$//g'`
 
+  dirname=$(basename "$testdir")
+
+  projectFile="${testdir}/${dirname}.csproj"
+
   echo "Executing unit tests in $testdir"
-  #dotnet test -f netcoreapp1.0 $DOTNET_TEST_ARGS $testdir
-  dotnet test $DOTNET_TEST_ARGS $testdir
+  dotnet test $DOTNET_TEST_ARGS --no-build $projectFile
 
   if [ "$project" = "Hystrix.Dotnet.AspNet" ]; then
-	seachdirs=$testdir/bin/$CONFIG/net451
+	searchdirs=$testdir/bin/$CONFIG/net452
   else
-	seachdirs=$testdir/bin/$CONFIG/netcoreapp1.0
+	searchdirs=$testdir/bin/$CONFIG/netcoreapp1.1
   fi
 
   echo "Calculating coverage with OpenCover for $project"
   $OPENCOVER \
     -target:"c:\Program Files\dotnet\dotnet.exe" \
-    -targetargs:"test $DOTNET_TEST_ARGS $testdir" \
+    -targetargs:"test $DOTNET_TEST_ARGS --no-build $projectFile" \
     -mergeoutput \
     -hideskipped:File \
     -output:$coverage/coverage.xml \
