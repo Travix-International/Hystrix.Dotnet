798f49369c0472e0feae8c5864c5ad0dc197e6fc
markvincze <mrk.vincze@gmail.com>
2017-11-12 16:15:45 +0100

    Add script for updating the gh-pages branch

--- releaseDocs.sh
+++ releaseDocs.sh
@@ -1,4 +1,29 @@
+#!/bin/sh
+set -e
+
 export VSINSTALLDIR="C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional"
 export VisualStudioVersion="15.0"
 
-docfx -t ./_exported_templates/default ./docs/docfx.json --serve
+docfx -t ./_exported_templates/default ./docs/docfx.json
+
+SOURCE_DIR=$PWD
+TEMP_REPO_DIR=$PWD/../Hystrix.Dotnet-gh-pages
+
+echo "Removing temporary doc directory $TEMP_REPO_DIR"
+rm -rf $TEMP_REPO_DIR
+mkdir $TEMP_REPO_DIR
+
+echo "Cloning the repo with the gh-pages branch"
+git clone git@github.com:Travix-International/Hystrix.Dotnet.git --branch gh-pages $TEMP_REPO_DIR
+
+echo "Clear repo directory"
+cd $TEMP_REPO_DIR
+git rm -r *
+
+echo "Copy documentation into the repo"
+cp -r $SOURCE_DIR/docs/_site/* .
+
+echo "Push the new docs to the remote branch"
+git add . -A
+git commit -m "Update generated documentation"
+git push origin gh-pages
