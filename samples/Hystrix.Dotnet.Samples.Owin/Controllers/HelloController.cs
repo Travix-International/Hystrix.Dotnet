69d1da2de1f9e70eebd90b4d43c085a6913b636f
klettier <lettier.kevin@gmail.com>
2017-12-03 13:17:31 +0100

    Refactor remove DI and delegate, Include all samples, fix AspNet sample references

--- samples/Hystrix.Dotnet.Samples.Owin/Controllers/HelloController.cs
+++ samples/Hystrix.Dotnet.Samples.Owin/Controllers/HelloController.cs
@@ -1,24 +1,43 @@
-﻿using System.Threading;
+﻿using Hystrix.Dotnet.WebConfiguration;
+using System;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace Hystrix.Dotnet.Samples.Owin.Controllers
 {
-    public delegate Task<string> GetBreakerCommand (CancellationToken cancellationToken);
-
     //api/hello
     public class HelloController : ApiController
     {
-        private readonly GetBreakerCommand getBreakerCommand;
+        private readonly IHystrixCommand hystrixCommand;
+        static readonly Random rnd = new Random();
 
-        public HelloController(GetBreakerCommand getBreakerCommand)
+        public HelloController()
         {
-            this.getBreakerCommand = getBreakerCommand;
+            var helper = new AspNetHystrixCommandFactoryHelper();
+
+            var factory = helper.CreateFactory();
+
+            hystrixCommand = factory.GetHystrixCommand("TestGroup", "TestCommand");
         }
 
         public async Task<IHttpActionResult> Get(CancellationToken cancellationToken)
         {
-            var result = await getBreakerCommand(cancellationToken);
+            var result = await hystrixCommand.ExecuteAsync(
+                    async () =>
+                    {
+                        // Here we could do a potentially failing operation, like calling an external service.
+                        await Task.Delay(rnd.Next(100, 200));
+
+                        if (rnd.Next(2) == 0)
+                        {
+                            throw new Exception("Test exception. Hystrix will catch this and return the FallbackResult instead.");
+                        }
+
+                        return "ExpensiveResult";
+                    },
+                    () => Task.FromResult("FallbackResult"),
+                    CancellationTokenSource.CreateLinkedTokenSource(cancellationToken));
 
             return Ok($"Hello! Result: {result}");
         }
--- samples/Hystrix.Dotnet.Samples.Owin/Program.cs
+++ samples/Hystrix.Dotnet.Samples.Owin/Program.cs
@@ -1,13 +1,7 @@
-﻿using Hystrix.Dotnet.Samples.Owin.Controllers;
-using Hystrix.Dotnet.WebConfiguration;
-using Microsoft.Owin.Hosting;
+﻿using Microsoft.Owin.Hosting;
 using Owin;
-using SimpleInjector;
-using SimpleInjector.Lifestyles;
 using System;
 using System.Configuration;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace Hystrix.Dotnet.Samples.Owin
@@ -42,51 +36,9 @@ namespace Hystrix.Dotnet.Samples.Owin
                 defaults: new { id = RouteParameter.Optional }
             );
 
-            SetupDI(config);
-
             app.UseHystrixStream();
 
             app.UseWebApi(config);
-        }
-
-        static void SetupDI(HttpConfiguration conf)
-        {
-            var container = new Container();
-            container.Options.DefaultScopedLifestyle = new AsyncScopedLifestyle();
-
-            var helper = new AspNetHystrixCommandFactoryHelper();
-
-            var factory = helper.CreateFactory();
-
-            container.Register<GetBreakerCommand>(() =>
-                (cancel => GetBreakerCommand(factory.GetHystrixCommand("TestGroup", "TestCommand"), cancel)), Lifestyle.Scoped);
-
-            container.Verify();
-
-            conf.DependencyResolver =
-                new SimpleInjector.Integration.WebApi.SimpleInjectorWebApiDependencyResolver(container);
-        }
-
-        static readonly Random rnd = new Random();
-
-        static Task<string> GetBreakerCommand(IHystrixCommand hystrixCommand, CancellationToken cancellationToken)
-        {
-            return 
-                hystrixCommand.ExecuteAsync(
-                    async () =>
-                    {
-                        // Here we could do a potentially failing operation, like calling an external service.
-                        await Task.Delay(rnd.Next(100, 200));
-
-                        if (rnd.Next(2) == 0)
-                        {
-                           throw new Exception("Test exception. Hystrix will catch this and return the FallbackResult instead.");
-                        }
-
-                        return "ExpensiveResult";
-                    },
-                    () => Task.FromResult("FallbackResult"),
-                    CancellationTokenSource.CreateLinkedTokenSource(cancellationToken));
         }
     }
 }
